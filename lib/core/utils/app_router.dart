b0b4aa1984e39ae76f2a2b95c2baed6df430d3e7
louaysalh <75487275+louaysalh@users.noreply.github.com>
2026-02-17 21:12:26 +0200

    create custom book details app bar

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,13 +1,19 @@
+import 'package:bookly/features/home/presentation/views/book_details_view.dart';
 import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static final kHomeView = '/homeView';
+  static final kBookDetails = '/bookDetailsView';
   static final router = GoRouter(
     routes: [
       GoRoute(path: '/', builder: (context, state) => const SplashView()),
       GoRoute(path: kHomeView, builder: (context, state) => const HomeView()),
+      GoRoute(
+        path: kBookDetails,
+        builder: (context, state) => const BookDetailsView(),
+      ),
     ],
   );
 }
--- lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,64 +1,76 @@
 import 'package:bookly/constants.dart';
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        AspectRatio(
-          aspectRatio: 2.5 / 4,
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              color: Colors.red,
-              image: DecorationImage(
-                image: AssetImage(AssetsData.testImage),
-                fit: BoxFit.fill,
-              ),
-            ),
-          ),
-        ),
-        const SizedBox(width: 30),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: Text(
-                  'Harry Potter and the Goblet of Fire',
-                  style: Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
-                  maxLines: 2,
-                  overflow: TextOverflow
-                      .ellipsis, // when overflow happen put .... instead
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.kBookDetails);
+      },
+      child: SizedBox(
+        height: 125,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.5 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.red,
+                  image: DecorationImage(
+                    image: AssetImage(AssetsData.testImage),
+                    fit: BoxFit.fill,
+                  ),
                 ),
               ),
-              const SizedBox(height: 3),
-              const Text('J.K. Rowling', style: Styles.textStyle14),
-              const SizedBox(height: 3),
-
-              Row(
+            ),
+            const SizedBox(width: 30),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    '19.99 ',
-                    style: Styles.textStyle20.copyWith(
-                      fontWeight: FontWeight.bold,
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * .5,
+                    child: Text(
+                      'Harry Potter and the Goblet of Fire',
+                      style: Styles.textStyle20.copyWith(
+                        fontFamily: kGtSectraFine,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow
+                          .ellipsis, // when overflow happen put .... instead
                     ),
                   ),
-                  const Spacer(),
-                  const BookRating(),
+                  const SizedBox(height: 3),
+                  const Text('J.K. Rowling', style: Styles.textStyle14),
+                  const SizedBox(height: 3),
+
+                  Row(
+                    children: [
+                      Text(
+                        '19.99 ',
+                        style: Styles.textStyle20.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Spacer(),
+                      const BookRating(),
+                    ],
+                  ),
                 ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
