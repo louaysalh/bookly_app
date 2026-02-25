96b62bf5f4e01face4aabc46ef25e842ee692b9a
louaysalh <75487275+louaysalh@users.noreply.github.com>
2026-02-25 17:13:45 +0200

    create api service

---
+++ lib/features/home/data/repos/home_repo_implmentation.dart
@@ -0,0 +1,18 @@
+import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImplmentation implements HomeRepo {
+  @override
+  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<BookModel>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+}
